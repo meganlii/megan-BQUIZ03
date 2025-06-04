0d2a539db2e686bd11ffefe6e2df451878948f26
meganli <lovecezanne@gmail.com>
2025-06-04 15:12:42 +0800

    0604更新

--- member/header.php
+++ member/header.php
@@ -1,5 +1,12 @@
+
+<!-- 頁首：區分兩個 logo nav -->
+<!-- .nav container：五個a box -->
 <div id='header'>
+
+    <!-- logo -->
     <div class='logo'></div>
+
+    <!-- nav -->
     <div class="nav">
         <a href="index.php">首頁</a>
         <a href="member_center.php">會員中心</a>
